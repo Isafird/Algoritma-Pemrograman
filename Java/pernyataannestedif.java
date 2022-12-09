5aa16d33eddba8225384c8a1049a39e01282c35a
Isafird <muhamadisafirdaus123@gmail.com>
2022-12-09 19:17:18 +0700

    upload nestedif

---
+++ Java/pernyataannestedif.java
@@ -0,0 +1,38 @@
+package Java;
+
+import java.util.Scanner;
+
+public class pernyataannestedif {
+    public static void main(String[] args) {
+
+        int belanjaan, diskon, bayar;
+        String kartu;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Apakah ada kartu member ?");
+        kartu = scan.nextLine();
+        System.out.print("Total belanjaan:");
+        belanjaan = scan.nextInt();
+
+        if(kartu.equalsIgnoreCase("ya")) {
+            if(belanjaan > 500000) {
+                diskon = 5000;
+            }   
+            else if(belanjaan > 100000) {
+                diskon = 10000;
+            }
+            else {
+                diskon = 0;
+            }
+        }else {
+            if(belanjaan > 100000) {
+                diskon = 5000;
+            }
+            else {
+                diskon = 0;
+            }
+        }
+        bayar = belanjaan - diskon;
+        System.out.println("Total bayar Rp " + bayar);
+        }
+    }
